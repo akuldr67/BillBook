cee02741dcaf4fc69cba44a4904af8dd57d818a1
Akul Gupta <akulodr67@gmail.com>
2020-09-04 01:40:14 +0530

    add customer and add item feature added

--- JS_BillBook/index.js
+++ JS_BillBook/index.js
@@ -157,17 +157,73 @@ const toggleModal = () => {
         document.querySelector('.modal').classList.toggle('show-modal');
 }
 
+const submitCustomer = async (event) => {
+    // console.log(event.target.parentNode);
+    let newCustomer = {
+        name: "",
+        email: "",
+        contact: ""
+    };
+    newCustomer.name = document.getElementById('NewCustomerName').value;
+    newCustomer.email = document.getElementById('NewCustomerEmail').value;
+    newCustomer.contact = document.getElementById('NewCustomerPhone').value;
+
+    try{
+        const result = await fetch('https://rzp-training.herokuapp.com/team1/customers',{
+            method: "POST",
+            body: JSON.stringify(newCustomer),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(res => {
+            console.log(res.status);
+            if(res.status === 200){
+                alert('Customer Added Successfully');
+            }else{
+                alert('Unable to add Customer');
+            }
+            toggleModal();
+            clearRightPanel();
+            renderCustomers();
+        });
+    }catch (error){
+        console.log(error);
+        alert('Unable to add Customer');
+        toggleModal();
+    };
+};
+
 const addCustomer = () => {
     clearModal();
     const markup = `
     <span class = "close-button"> &times; </span>
     <div class = "customer-modal-data">
-        <h3> hello in customer modal</h3>
+        <h2 class = "customer-modal-heading"> New Customer</h2>
+            <table class = "addCustomerTable">
+            <tr>
+            <td> <label for= "NewCustomerName"> Name </label> <br>
+            <input type = "text" id = "NewCustomerName" name = "NewCustomerName" />
+            </td>
+            <td>
+            <label for = "NewCustomerPhone"> Phone </label> <br>
+            <input type = "text" id = "NewCustomerPhone" name = "NewCustomerPhone" /> 
+            </td>
+            </tr>
+            <tr>
+            <td> <label for= "NewCustomerEmail"> Email </label> <br>
+            <input type = "text" id = "NewCustomerEmail" name = "NewCustomerEmail" />
+            </td>
+            <td>
+            <button class = "customerSubmit"> Save Customer </button>
+            </td>
+            </tr>
+            </table>
     </div>
     `;
     document.querySelector('.modal-content').insertAdjacentHTML('afterbegin',markup);
     toggleModal();
     document.querySelector('.close-button').addEventListener('click',toggleModal);
+    document.querySelector('.customerSubmit').addEventListener('click',submitCustomer);
 }
 
 const renderCustomers = () => {
@@ -188,17 +244,61 @@ const renderCustomers = () => {
     document.querySelector('.btn-customer').addEventListener('click',addCustomer);
 };
 
+const submitItem = async (event) => {
+    let newItem = {
+        name: "",
+        description: "",
+        amount: "",
+        currency: "INR"
+    };
+    newItem.name = document.getElementById('NewItemName').value;
+    newItem.amount = document.getElementById('NewItemPrice').value*100;
+    newItem.description = document.getElementById('NewItemDesc').value;
+
+    try{
+        const result = await fetch('https://rzp-training.herokuapp.com/team1/items',{
+            method: "POST",
+            body: JSON.stringify(newItem),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(res => {
+            console.log(res.status);
+            if(res.status === 200){
+                alert('Item Added Successfully');
+            }else{
+                alert('Unable to add Item');
+            }
+            toggleModal();
+            clearRightPanel();
+            renderEntities();
+        });
+    }catch (error){
+        console.log(error);
+        alert('Unable to add Item');
+        toggleModal();
+    };
+};
+
 const addItem = () => {
     clearModal();
     const markup = `
     <span class = "close-button"> &times; </span>
     <div class = "item-modal-data">
-        <h3> hello in item modal</h3>
+        <h2 class = "item-modal-heading"> New Item</h2>
+        <label for = "NewItemName"> Name </label> <br/>
+        <input type = "text" id = "NewItemName" name = "NewItemName" /> <br/>
+        <label for = "NewItemPrice"> Price (in Rs.)</label> <br/>
+        <input type = "text" id = "NewItemPrice" name = "NewItemPrice" /> <br/>
+        <label for = "NewItemDesc"> Description </label> <br/>
+        <textarea type = "text" id = "NewItemDesc" name = "NewItemDesc" /> </textarea> <br/>
+        <button class = "itemSubmit"> Save Item </button>
     </div>
     `;
     document.querySelector('.modal-content').insertAdjacentHTML('afterbegin',markup);
     toggleModal();
     document.querySelector('.close-button').addEventListener('click',toggleModal);
+    document.querySelector('.itemSubmit').addEventListener('click',submitItem);
 };
 
 const renderEntities = () => {
