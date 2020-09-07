2f92d4dcad965ddb16658e4626443d84164688a1
Akul Gupta <akulodr67@gmail.com>
2020-09-07 15:07:58 +0530

    react project initiated

---
+++ billbook_react/.eslintrc.js
@@ -0,0 +1,13 @@
+//Path: /.eslintrc.js
+
+module.exports = {
+    extends: ['airbnb'],
+    rules: {
+      'react/jsx-filename-extension': 'off',
+      //You can override any rules you want
+    },
+    env: {
+        browser: true,
+        node: true
+    }
+  };
---
+++ billbook_react/.prettierrc.js
@@ -0,0 +1,10 @@
+//Path: /.prettierrc.js
+
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    // Override any other rules you want
+  };
+  
